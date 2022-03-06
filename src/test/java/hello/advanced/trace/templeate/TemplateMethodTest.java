08f8c9c5a1df749ed1957a88bb638b759ac38205
hyungkiShin <tkaqkeldk@nate.com>
2022-03-06 20:24:12 +0900

    [Template methods] 패턴 -> 다형성을 사용해서 변하는 부분과 변하지 않는 부분을 분리

---
+++ src/test/java/hello/advanced/trace/templeate/TemplateMethodTest.java
@@ -0,0 +1,47 @@
+package hello.advanced.trace.templeate;
+
+import hello.advanced.trace.templeate.code.AbstractTemplate;
+import hello.advanced.trace.templeate.code.SubClassLogic1;
+import hello.advanced.trace.templeate.code.SubClassLogic2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class TemplateMethodTest {
+
+    @Test
+    void templateMethodV0() {
+        logic1();
+        logic2();
+    }
+
+    public void logic1() {
+        long startTime = System.currentTimeMillis();
+        // 비즈니스 로직 실행
+        log.info("비즈니스 로직1 실행");
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+
+    public void logic2() {
+        long startTime = System.currentTimeMillis();
+        // 비즈니스 로직 실행
+        log.info("비즈니스 로직1 실행");
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+
+    /**
+     * 템플릿 메서드 패턴 적용
+     */
+    @Test
+    void templateMethodV1() {
+        AbstractTemplate template1 = new SubClassLogic1();
+        template1.execute();
+
+        AbstractTemplate template2 = new SubClassLogic2();
+        template2.execute();
+    }
+}
